a1789c291414c8e3c043988318f8a1d4f1f0eee5
gkishore1010 <gayatri.kishore@gmail.com>
2016-04-16 00:00:01 -0700

    Update and rename ServiceDAO.java to BookingDAO.java

---
+++ app/src/main/java/com/cmu/tiegen/db/BookingDAO.java
@@ -0,0 +1,6 @@
+package com.cmu.tiegen.db;
+
+//create booking inner class
+//read booking inner class
+//update booking inner class
+//delete booking inner class
--- app/src/main/java/com/cmu/tiegen/db/ServiceDAO.java
+++
@@ -1,2 +0,0 @@
-package com.cmu.tiegen.db;
-
