0653168de4e5b607254267abbe1ad5850e280095
gkishore1010 <gayatri.kishore@gmail.com>
2016-04-27 01:33:59 -0700

    Fixed Dao

--- app/src/main/java/com/cmu/tiegen/db/BookmarkDao.java
+++ app/src/main/java/com/cmu/tiegen/db/BookmarkDao.java
@@ -3,26 +3,23 @@ package db;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class BookmarkDao {
-	public static class CreateBookmark extends Db {
-		public void create(int userId, int serviceProviderId) throws SQLException {
-			// TODO: bookmark_insert
-			PreparedStatement stmt = this.connection.prepareStatement(dbProps.getProperty("bookmark_insert"));
-			stmt.setInt(1, userId);
-			stmt.setInt(2, serviceProviderId);
-			stmt.executeUpdate();
-			stmt.close();
-		}
-		
+public class BookmarkDao extends Db {
+
+	public void create(int userId, int serviceProviderId) throws SQLException {
+		// TODO: bookmark_insert
+		PreparedStatement stmt = this.connection.prepareStatement(dbProps.getProperty("bookmark_insert"));
+		stmt.setInt(1, userId);
+		stmt.setInt(2, serviceProviderId);
+		stmt.executeUpdate();
+		stmt.close();
 	}
 
-	public static class DeleteBookmark extends Db {
-		public void delete(int bookmarkId) throws SQLException {
-			// TODO: bookmark_delete
-			PreparedStatement stmt = this.connection.prepareStatement(dbProps.getProperty("bookmark_delete"));
-			stmt.setInt(1, bookmarkId);
-			stmt.executeUpdate();
-			stmt.close();
-		}
+	public void delete(int bookmarkId) throws SQLException {
+		// TODO: bookmark_delete
+		PreparedStatement stmt = this.connection.prepareStatement(dbProps.getProperty("bookmark_delete"));
+		stmt.setInt(1, bookmarkId);
+		stmt.executeUpdate();
+		stmt.close();
 	}
+
 }
